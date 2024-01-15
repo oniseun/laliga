947603bbe301a7f9c49edb7d76925456c11e375c
Victor Oni <seunoni34@gmail.com>
2024-01-15 05:25:33 +0100

    chore: refactor main, add vlidation pipes and logger message types

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('LaLiga API')
     .setDescription('API documentation for LaLiga News and Standings')
     .setVersion('1.0')
-    .addTag('LaLiga News Stadings')
+    .addTag('LaLiga News Standings')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3001);
 }
 bootstrap();
